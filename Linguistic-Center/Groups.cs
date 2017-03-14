928767cacfd74712dc9bfb782a83097d3d09bb46
Мария Альчикова <msalchikova@edu.hse.ru>
2017-03-14 22:50:35 +0300

    Added the class "Groups" which discribes the main characteristics of a language group like language, level of knowledge, age, course location.

---
+++ Linguistic-Center/Groups.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace language_center
+{
+    class Groups
+    {
+        private string _language;
+        private string _level;
+        private int _age;
+        private string _metro;
+
+
+
+        public string Language
+        {
+            get
+            {
+                return _language;
+            }
+            set
+            {
+                _language = value;
+            }
+        }
+
+        public string Level
+        {
+            get
+
+            {
+                return _level;
+            }
+            set
+            {
+                _level = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public string Metro
+        {
+            get
+
+            {
+                return _metro;
+            }
+            set
+            {
+                _metro = value;
+            }
+        }
+
+
+        public Groups(string lng, string lvl, int age, string met)
+        {
+
+            Language = lng;
+            Level = lvl;
+            Age = age;
+            Metro = met;
+        }
+    }
+}
