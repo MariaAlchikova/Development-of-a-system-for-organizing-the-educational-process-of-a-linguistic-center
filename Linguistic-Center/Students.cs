b50ab4c42f6c7d8659fd759639a7b10d1d878407
Мария Альчикова <msalchikova@edu.hse.ru>
2017-04-04 02:39:04 +0300

    Added a new class "Students" which discribes students of the Linguistic Center. Changed design of the MainWindow.

---
+++ Linguistic-Center/Students.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguistic_Center
+{
+    class Students
+    {
+        private string _secondName;
+        private string _firstName;
+        private string _monthlyPayment; // "yes" if a student paid for courses, "no" if he did not paid
+        private float _mark;
+
+
+
+
+        public string SecondName
+        {
+            get
+            {
+                return _secondName;
+            }
+            set
+            {
+                _secondName = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        public float Mark
+        {
+            get
+            {
+                return _mark;
+            }
+            set
+            {
+                _mark = value;
+            }
+        }
+
+        public string MonthlyPayment
+        {
+            get
+            {
+                return _monthlyPayment;
+            }
+            set
+            {
+                _monthlyPayment = value;
+            }
+        }
+        public Students(string fName, string sName, float mark, string pay)
+        {
+            SecondName = sName;
+            FirstName = fName;
+            MonthlyPayment = pay;
+            Mark = mark;
+        }
+
+        
+    }
+}
