f4c062c473ac3e8b8b6c9ea8c38b923fa81a67d1
Мария Альчикова <msalchikova@edu.hse.ru>
2017-04-18 22:09:39 +0300

    Added the class "Teachers" which describes characteristics of the teachers in the center.

---
+++ Linguistic-Center/Teachers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguistic_Center
+{
+    class Teachers
+    {
+        private string _name;
+        private string _foreignLanguage; 
+        private float _groupNumber;
+
+
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string ForeignLanguage
+        {
+            get
+            {
+                return _foreignLanguage;
+            }
+            set
+            {
+                _foreignLanguage = value;
+            }
+        }
+
+        public float GroupNumber
+        {
+            get
+            {
+                return _groupNumber;
+            }
+            set
+            {
+                _groupNumber = value;
+            }
+        }
+
+        
+        public Teachers(string teachersName, string fLang, float gNumber)
+        {
+            Name = teachersName;
+            ForeignLanguage = fLang;
+            GroupNumber = gNumber;
+        }
+    }
+}
